9e7fa25fffe3a919482fede635d7e03942891082
Joe Panetta <jlpanetta1681@icloud.com>
2021-11-16 23:14:23 -0500

    working on the carousel

--- src/App.js
+++ src/App.js
@@ -5,17 +5,27 @@ function App() {
   return (
     <Carousel showThumbs={false}>
       <div>
-        <img src="https://picsum.photos/200/300" />
+        <img src="./images/banner-img1.jpeg" alt="banner1" />
         <button className="legend">Legend 3</button>
 
       </div>
       <div>
-        <img src="https://picsum.photos/200/300" />
+        <img src="./images/banner-img2.jpeg" alt="banner2" />
         <button className="legend">Legend 3</button>
 
       </div>
       <div>
-        <img src="https://picsum.photos/200/300" />
+        <img src="./images/banner-img3.jpeg" alt="banner3" />
+        <p>Most platforms place little value on you and your time, pay you peanuts, and take you for granted. Feel recognized. Feel prioritized. Feel supported. Get Anthology today.</p>
+        <button className="legend">Legend 3</button>
+      </div>
+      <div>
+        <img src="./images/banner-img4.jpeg" alt="banner4" />
+        <p>Most platforms place little value on you and your time, pay you peanuts, and take you for granted. Feel recognized. Feel prioritized. Feel supported. Get Anthology today.</p>
+        <button className="legend">Legend 3</button>
+      </div>
+      <div>
+        <img src="./images/banner-img5.jpeg" alt="banner5" />
         <p>Most platforms place little value on you and your time, pay you peanuts, and take you for granted. Feel recognized. Feel prioritized. Feel supported. Get Anthology today.</p>
         <button className="legend">Legend 3</button>
       </div>
@@ -24,3 +34,4 @@ function App() {
 }
 
 export default App;
+
