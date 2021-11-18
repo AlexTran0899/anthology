d1dce3ff93c0ed21bd2457bcb7b0cbd080ebd93f
AlexTran0899 <76791231+AlexTran0899@users.noreply.github.com>
2021-11-17 19:14:09 -0600

    test

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <Carousel showThumbs={false}>
       <div>
         <img src="https://www.anthology-us.com/assets/images/banner-img1.jpg" alt="banner1" />
-        <button className="legend">Legend 1</button>
-
+        <button className="text-xl border-2">Legend 1</button>
       </div>
       <div>
         <img src="https://www.anthology-us.com/assets/images/banner-img2.jpg" alt="banner2" />
